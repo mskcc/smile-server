40ca7460333952a951ca4c8fac9660f033c010ce
Divya Madala <71040191+divyamadala30@users.noreply.github.com>
2021-01-20 15:58:58 -0500

    CmoRequestEntity now extends to IgoRequest
    Request node uses Long id for indexing
    CmoRequestEntity now extends to IgoRequest
    
    Signed-off-by: Divya Madala <71040191+divyamadala30@users.noreply.github.com>

--- model/src/main/java/org/mskcc/cmo/metadb/model/CmoRequestEntity.java
+++ model/src/main/java/org/mskcc/cmo/metadb/model/CmoRequestEntity.java
@@ -2,6 +2,8 @@ package org.mskcc.cmo.metadb.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.mskcc.cmo.shared.IgoRequest;
+import org.neo4j.ogm.annotation.GeneratedValue;
 import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Relationship;
@@ -11,9 +13,9 @@ import org.neo4j.ogm.annotation.Relationship;
  * @author ochoaa
  */
 @NodeEntity(label = "cmo_metadb_request")
-public class CmoRequestEntity {
-    @Id
-    private String requestId;
+public class CmoRequestEntity extends IgoRequest {
+    @Id @GeneratedValue
+    private Long id;
     @Relationship(type = "REQUEST_TO_SP", direction = Relationship.OUTGOING)
     private List<SampleManifestEntity> sampleManifestList;
     @Relationship(type = "PR_TO_REQUEST", direction = Relationship.INCOMING)
@@ -26,22 +28,70 @@ public class CmoRequestEntity {
      * CmoRequestEntity constructor.
      * @param requestId
      * @param sampleManifestList
-     * @param requestJson 
+     * @param requestJson
      */
-    public CmoRequestEntity(String requestId, List<SampleManifestEntity> sampleManifestList, 
+    public CmoRequestEntity(String requestId, List<SampleManifestEntity> sampleManifestList,
             CmoProjectEntity projectEntity, String requestJson) {
-        this.requestId = requestId;
+        super(requestId);
         this.sampleManifestList = sampleManifestList;
         this.projectEntity = projectEntity;
         this.requestJson = requestJson;
     }
 
-    public String getRequestId() {
-        return requestId;
+    /**
+     * CmoRequestEntity constructor
+     * @param requestId
+     * @param recipe
+     * @param projectManagerName
+     * @param piEmail
+     * @param labHeadName
+     * @param labHeadEmail
+     * @param investigatorName
+     * @param investigatorEmail
+     * @param dataAnalystName
+     * @param dataAnalystEmail
+     * @param otherContactEmails
+     * @param dataAccessEmails
+     * @param qcAccessEmails
+     * @param strand
+     * @param libraryType
+     * @param sampleManifestList
+     * @param projectEntity
+     * @param requestJson
+     */
+    public CmoRequestEntity(String requestId, String recipe, String projectManagerName,
+            String piEmail, String labHeadName, String labHeadEmail,
+            String investigatorName, String investigatorEmail, String dataAnalystName,
+            String dataAnalystEmail, String otherContactEmails, String dataAccessEmails,
+            String qcAccessEmails, String strand, String libraryType,
+            List<SampleManifestEntity> sampleManifestList,
+            CmoProjectEntity projectEntity, String requestJson) {
+        super(requestId,
+                recipe,
+                projectManagerName,
+                piEmail,
+                labHeadName,
+                labHeadEmail,
+                investigatorName,
+                investigatorEmail,
+                dataAnalystName,
+                dataAnalystEmail,
+                otherContactEmails,
+                dataAccessEmails,
+                qcAccessEmails,
+                strand,
+                libraryType);
+        this.sampleManifestList = sampleManifestList;
+        this.projectEntity = projectEntity;
+        this.requestJson = requestJson;
     }
 
-    public void setRequestId(String requestId) {
-        this.requestId = requestId;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public List<SampleManifestEntity> getSampleManifestList() {
@@ -51,7 +101,7 @@ public class CmoRequestEntity {
     public void setSampleManifestList(List<SampleManifestEntity> sampleManifestList) {
         this.sampleManifestList = sampleManifestList;
     }
-    
+
     public CmoProjectEntity getProjectEntity() {
         return projectEntity;
     }
@@ -67,15 +117,15 @@ public class CmoRequestEntity {
     public void setRequestJson(String requestJson) {
         this.requestJson = requestJson;
     }
-    
+
     /**
-     * 
+     *
      * @param sampleManifestEntity
      */
     public void addSampleManifest(SampleManifestEntity sampleManifestEntity) {
         if (sampleManifestList == null) {
             sampleManifestList = new ArrayList<>();
         }
-        sampleManifestList.add(sampleManifestEntity); 
+        sampleManifestList.add(sampleManifestEntity);
     }
 }
