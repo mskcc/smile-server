04a9dcc1e5a3d8b73b6ed6ab84ccd7f33735f82d
Divya Madala <71040191+divyamadala30@users.noreply.github.com>
2021-02-25 12:05:53 -0500

    Added missing attributes and fixed returned json format

--- model/src/main/java/org/mskcc/cmo/metadb/model/MetaDbRequest.java
+++ model/src/main/java/org/mskcc/cmo/metadb/model/MetaDbRequest.java
@@ -43,6 +43,8 @@ public class MetaDbRequest implements Serializable {
     protected String libraryType;
     protected List<RequestSample> requestSamples;
     protected List<String> pooledNormals;
+    protected boolean isCmoRequest;
+    protected boolean bicAnalysis;
 
     public MetaDbRequest() {}
 
@@ -65,13 +67,15 @@ public class MetaDbRequest implements Serializable {
      * @param libraryType
      * @param metaDbSampleList
      * @param requestJson
+     * @param bicAnalysis
+     * @param isCmoRequest
      */
     public MetaDbRequest(String requestId, String recipe, String projectManagerName,
             String piEmail, String labHeadName, String labHeadEmail,
             String investigatorName, String investigatorEmail, String dataAnalystName,
             String dataAnalystEmail, String otherContactEmails, String dataAccessEmails,
             String qcAccessEmails, String strand, String libraryType,
-            List<MetaDbSample> metaDbSampleList, String requestJson) {
+            List<MetaDbSample> metaDbSampleList, String requestJson, boolean bicAnalysis, boolean isCmoRequest) {
         this.requestId = requestId;
         this.recipe = recipe;
         this.projectManagerName = projectManagerName;
@@ -90,6 +94,8 @@ public class MetaDbRequest implements Serializable {
         this.metaDbSampleList = metaDbSampleList;
         this.metaDbProject = new MetaDbProject(requestId.split("_")[0]);
         this.requestJson = requestJson;
+        this.setBicAnalysis(bicAnalysis);
+        this.setCmoRequest(isCmoRequest);
     }
 
     public Long getId() {
@@ -287,6 +293,22 @@ public class MetaDbRequest implements Serializable {
 
     public void setPooledNormals(List<String> pooledNormals) {
         this.pooledNormals = pooledNormals;
+    }
+
+    public boolean isCmoRequest() {
+        return isCmoRequest;
+    }
+
+    public void setCmoRequest(boolean isCmoRequest) {
+        this.isCmoRequest = isCmoRequest;
+    }
+
+    public boolean isBicAnalysis() {
+        return bicAnalysis;
+    }
+
+    public void setBicAnalysis(boolean bicAnalysis) {
+        this.bicAnalysis = bicAnalysis;
     }
 
     @Override
