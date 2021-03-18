afda6988709ab5053f010d9d20ae1bca49296a1b
Angelica Ochoa <ochoaa@mskcc.org>
2021-03-18 16:48:39 -0400

    Update RequestController
    - sample/patient uuid now called sample/patient[MetaDbId]
    - request controller returns http status not found if request id does not exist
    - http headers return metadb schema version
    - designated metadb schema version in master pom properties
    - metadb schema version is defined in the master pom.xml which is autowired in the controller
      thanks to the maven properties plugin
    
    Co-authored-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>
    Co-authored-by: Divya Madala <71040191+divyamadala30@users.noreply.github.com>
    
    Signed-off-by: Angelica Ochoa <ochoaa@mskcc.org>

--- web/src/main/java/org/mskcc/cmo/metadb/web/RequestController.java
+++ web/src/main/java/org/mskcc/cmo/metadb/web/RequestController.java
@@ -4,10 +4,17 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.mskcc.cmo.metadb.model.web.PublishedMetaDbRequest;
 import org.mskcc.cmo.metadb.service.MetaDbRequestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +26,11 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 @RequestMapping(value = "/")
 @Api(tags = "request-controller", description = "Request Controller")
+@PropertySource("classpath:/maven.properties")
 public class RequestController {
+
+    @Value("${metadb.schema_version}")
+    private String metaDbSchemaVersion;
 
     @Autowired
     private MetaDbRequestService metaDbRequestService;
@@ -32,36 +43,84 @@ public class RequestController {
     /**
      * fetchRequestGET
      * @param requestId
+     * @return ResponseEntity
      */
     @ApiOperation(value = "Retrieve MetaDbRequest",
         nickname = "fetchMetaDbRequestGET")
     @RequestMapping(value = "/request/{requestId}",
         method = RequestMethod.GET,
         produces = "application/json")
-    public PublishedMetaDbRequest fetchMetaDbRequestGET(@ApiParam(value =
+    public ResponseEntity<PublishedMetaDbRequest> fetchMetaDbRequestGET(@ApiParam(value =
         "Retrieves MetaDbRequest from a RequestId.",
         required = true)
         @PathVariable String requestId) throws Exception {
-        return metaDbRequestService.getMetaDbRequest(requestId);
+        PublishedMetaDbRequest request = metaDbRequestService.getMetaDbRequest(requestId);
+        if (request == null) {
+            return requestNotFoundHandler("Request does not exist by id: " + requestId);
+        }
+        return ResponseEntity.ok()
+                .headers(responseHeaders())
+                .body(request);
     }
 
     /**
      * fetchRequestListPOST
      * @param requestIds
      * TODO properly set-up POST
+     * @return ResponseEntity
      */
     @ApiOperation(value = "Retrieves list of MetaDbRequest from a list of RequestIds.",
         nickname = "fetchMetaDbRequestListPOST")
     @RequestMapping(value = "/request",
         method = RequestMethod.POST,
         produces = "application/json")
-    public List<PublishedMetaDbRequest> fetchMetaDbRequestPOST(@ApiParam(value =
+    public ResponseEntity<List<PublishedMetaDbRequest>> fetchMetaDbRequestPOST(@ApiParam(value =
         "List of request ids", required = true, allowMultiple = true)
         @RequestBody List<String> requestIds) throws Exception {
         List<PublishedMetaDbRequest> requestList = new ArrayList<>();
         for (String requestId: requestIds) {
-            requestList.add(metaDbRequestService.getMetaDbRequest(requestId));
+            PublishedMetaDbRequest request = metaDbRequestService.getMetaDbRequest(requestId);
+            if (request != null) {
+                requestList.add(request);
+            }
         }
-        return requestList;
+        return ResponseEntity.ok()
+                .headers(responseHeaders())
+                .body(requestList);
+    }
+
+    /**
+     * fetchRequestGET
+     * @param requestId
+     * @return ResponseEntity
+     */
+    @ApiOperation(value = "Retrieve MetaDbRequest",
+        nickname = "fetchMetaDbRequestJsonGET")
+    @RequestMapping(value = "/requestJson/{requestId}",
+        method = RequestMethod.GET,
+        produces = "application/json")
+    public ResponseEntity<String> fetchMetaDbRequestJsonGET(@ApiParam(value =
+        "Retrieves MetaDbRequest from a RequestId.",
+        required = true)
+        @PathVariable String requestId) throws Exception {
+        PublishedMetaDbRequest request = metaDbRequestService.getMetaDbRequest(requestId);
+        if (request == null) {
+            return requestNotFoundHandler("Request not found by id: " + requestId);
+        }
+        return ResponseEntity.ok()
+                .headers(responseHeaders())
+                .body(request.getRequestJson());
+    }
+
+    private HttpHeaders responseHeaders() {
+        HttpHeaders headers  = new HttpHeaders();
+        headers.set("metadb-schema-version", metaDbSchemaVersion);
+        return headers;
+    }
+
+    private ResponseEntity requestNotFoundHandler(String message) {
+        Map<String, String> map = new HashMap<>();
+        map.put("message", message);
+        return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
     }
 }
