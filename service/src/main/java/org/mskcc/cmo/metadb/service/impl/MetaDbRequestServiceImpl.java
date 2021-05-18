f2712dc0985cef999280c3f4a61380615247bed6
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2021-05-18 15:14:30 -0400

    do not log duplicate requests if seen in the same 24 hour window
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

--- service/src/main/java/org/mskcc/cmo/metadb/service/impl/MetaDbRequestServiceImpl.java
+++ service/src/main/java/org/mskcc/cmo/metadb/service/impl/MetaDbRequestServiceImpl.java
@@ -4,7 +4,11 @@ import java.io.File;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.mskcc.cmo.common.FileUtil;
@@ -38,10 +42,12 @@ public class MetaDbRequestServiceImpl implements MetaDbRequestService {
     private SampleService sampleService;
 
     @Autowired
-    FileUtil fileUtil;
+    private FileUtil fileUtil;
 
+    // 24 hours in milliseconds
+    private final Integer TIME_ADJ_24HOURS_MS = 24 * 60 * 60 * 1000;
+    private Map<String, Date> loggedExistingRequests = new HashMap<>();
     private static final Log LOG = LogFactory.getLog(MetaDbRequestServiceImpl.class);
-
     private static final String REQ_FAILURES_FILE_HEADER = "DATE\tREASON\tMESSAGE\n";
 
     @Override
@@ -64,10 +70,30 @@ public class MetaDbRequestServiceImpl implements MetaDbRequestService {
             metaDbRequestRepository.save(request);
             return true;
         } else {
-            File requestFailureFile = fileUtil.getOrCreateFileWithHeader(
-                    metadbRequestFailuresFilepath, REQ_FAILURES_FILE_HEADER);
-            fileUtil.writeToFile(requestFailureFile,
-                    generateRequestFailureRecord("Request already exists", request.getRequestJson()));
+            // if request has not been logged before then save request to request logger file
+            // otherwise check if new timestamp occurs within 24 hours since the last time
+            // the same request was seen. If it does not then save request to logger file
+            Date newTimestamp = new Date();
+            Boolean logRequest = Boolean.FALSE;
+            if (!loggedExistingRequests.containsKey(savedRequest.getRequestId())) {
+                loggedExistingRequests.put(savedRequest.getRequestId(), newTimestamp);
+                logRequest = Boolean.TRUE;
+            } else {
+                // check if new timestamp occurs within 24 hours of the reference timestamp
+                // if check does not pass then log the request to the logger file
+                Date referenceTimestamp = loggedExistingRequests.get(savedRequest.getRequestId());
+                if (!timestampWithin24Hours(referenceTimestamp, newTimestamp)) {
+                    logRequest = Boolean.TRUE;
+                    loggedExistingRequests.put(savedRequest.getRequestId(), newTimestamp);
+                }
+            }
+
+            if (logRequest) {
+                File requestFailureFile = fileUtil.getOrCreateFileWithHeader(
+                        metadbRequestFailuresFilepath, REQ_FAILURES_FILE_HEADER);
+                fileUtil.writeToFile(requestFailureFile,
+                        generateRequestFailureRecord("Request already exists", request.getRequestJson()));
+            }
             return false;
         }
     }
@@ -103,5 +129,19 @@ public class MetaDbRequestServiceImpl implements MetaDbRequestService {
                     .getSampleMetadataList());
         }
         return new PublishedMetaDbRequest(metaDbRequest, samples);
+    }
+
+    /**
+     * Returns true if new timestamp occurs within 24 hours of the reference timestamp.
+     * @param referenceTimestamp
+     * @param newTimestamp
+     * @return Boolean
+     */
+    private Boolean timestampWithin24Hours(Date referenceTimestamp, Date newTimestamp) {
+        // create reference timestamp shifted 24 hours in milliseconds
+        Calendar adjustedReferenceTimestamp = Calendar.getInstance();
+        adjustedReferenceTimestamp.setTime(referenceTimestamp);
+        adjustedReferenceTimestamp.add(Calendar.MILLISECOND, TIME_ADJ_24HOURS_MS);
+        return newTimestamp.before(adjustedReferenceTimestamp.getTime());
     }
 }
