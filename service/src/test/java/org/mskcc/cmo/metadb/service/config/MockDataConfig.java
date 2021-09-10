8679f6ff3462bfef12484e73e297bc408364def1
Divya Madala <71040191+divyamadala30@users.noreply.github.com>
2021-09-10 13:44:18 -0400

    skeleton unit test
    Signed-off-by: Divya Madala <71040191+divyamadala30@users.noreply.github.com>

---
+++ service/src/test/java/org/mskcc/cmo/metadb/service/config/MockDataConfig.java
@@ -0,0 +1,10 @@
+package org.mskcc.cmo.metadb.service.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "org.mskcc.cmo.common.*")
+public class MockDataConfig {
+    
+}
