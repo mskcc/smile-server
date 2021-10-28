2cdc8bbe822158a911bd6e090667d2e94786b1ef
Divya Madala <71040191+divyamadala30@users.noreply.github.com>
2021-10-28 10:14:42 -0400

    checkstyle fixes
    Signed-off-by: Divya Madala <71040191+divyamadala30@users.noreply.github.com>

--- model/src/main/java/org/mskcc/cmo/metadb/model/internal/CrdbIdMappingModel.java
+++ model/src/main/java/org/mskcc/cmo/metadb/model/internal/CrdbIdMappingModel.java
@@ -1,44 +1,47 @@
 package org.mskcc.cmo.metadb.model.internal;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class CrdbIdMappingModel {
     @Id
-    private String DMP_ID;
-    private String CMO_ID;
-    private String PT_MRN;
-    private String API_DMP_ID;
-
-    public String getCMO_ID() {
-        return CMO_ID;
+    private String dmpId;
+    private String cmoId;
+    private String ptMrn;
+    private String apiDmpId;
+    
+    public String getDmpId() {
+        return dmpId;
+    }
+    
+    public void setDmpId(String dmpId) {
+        this.dmpId = dmpId;
+    }
+    
+    public String getCmoId() {
+        return cmoId;
+    }
+    
+    public void setCmoId(String cmoId) {
+        this.cmoId = cmoId;
     }
 
-    public void setCMO_ID(String cMO_ID) {
-        CMO_ID = cMO_ID;
+    public String getPtMrn() {
+        return ptMrn;
     }
 
-    public String getDMP_ID() {
-        return DMP_ID;
+    public void setPtMrn(String ptMrn) {
+        this.ptMrn = ptMrn;
     }
 
-    public void setDMP_ID(String dMP_ID) {
-        DMP_ID = dMP_ID;
+    public String getApiDmpId() {
+        return apiDmpId;
     }
 
-    public String getPT_MRN() {
-        return PT_MRN;
+    public void setApiDmpId(String apiDmpId) {
+        this.apiDmpId = apiDmpId;
     }
 
-    public void setPT_MRN(String pT_MRN) {
-        PT_MRN = pT_MRN;
-    }
-
-    public String getAPI_DMP_ID() {
-        return API_DMP_ID;
-    }
-
-    public void setAPI_DMP_ID(String aPI_DMP_ID) {
-        API_DMP_ID = aPI_DMP_ID;
-    }
+    
 }
--- service/src/main/java/org/mskcc/cmo/metadb/service/impl/CrdbIdMappingServiceImpl.java
+++ service/src/main/java/org/mskcc/cmo/metadb/service/impl/CrdbIdMappingServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CrdbIdMappingServiceImpl implements CrdbIdMappingService{
+public class CrdbIdMappingServiceImpl implements CrdbIdMappingService {
 
     @Autowired
     private CrdbIdRepository crdbIdRepository;
