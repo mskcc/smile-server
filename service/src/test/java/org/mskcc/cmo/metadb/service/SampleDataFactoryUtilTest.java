5f238f7e63f72a781040b9fb5024dcc8c8f44c5c
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2022-01-05 15:19:39 -0500

    Added more checks for DMP conversion

--- service/src/test/java/org/mskcc/cmo/metadb/service/SampleDataFactoryUtilTest.java
+++ service/src/test/java/org/mskcc/cmo/metadb/service/SampleDataFactoryUtilTest.java
@@ -1,6 +1,7 @@
 package org.mskcc.cmo.metadb.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
 import java.util.Map;
 import junit.framework.Assert;
 import org.junit.jupiter.api.Test;
@@ -23,6 +24,8 @@ public class SampleDataFactoryUtilTest {
     private final ObjectMapper mapper = new ObjectMapper();
     @Autowired
     private MockDataUtils mockDataUtils;
+    private Map<String, SampleMetadata> expectedConvertedDmpSampleValues
+            = initExpectedConvertedDmpSampleValues();
 
     /**
      * Tests that deserialization of igo sample jsons succeeds and that the sample
@@ -53,11 +56,63 @@ public class SampleDataFactoryUtilTest {
             String cmoPatientId = mockDataUtils.mockedDmpPatientMapping.get(dmpSample.getDmpPatientId());
             SampleMetadata sampleMetadata =
                     SampleDataFactory.buildNewSampleMetadataFromDmpSample(cmoPatientId, dmpSample);
-            // verify that collection year was resolved correctly
-            String paddedCollectionYear = " " + sampleMetadata.getCollectionYear() + " ";
-            Assert.assertTrue(dmpSample.getDateTumorSequencing().contains(paddedCollectionYear));
-            //TODO add tests to verify other specific values were resolved correctly
-            // such as sampleClass, sampleType, sex, and tumorOrNormal
+
+            // verify that certain values were resolved correctly
+            SampleMetadata expected = expectedConvertedDmpSampleValues.get(dmpSample.getDmpSampleId());
+            if (!sampleMetadata.getCollectionYear().equals(expected.getCollectionYear())
+                    || !sampleMetadata.getSampleClass().equals(expected.getSampleClass())
+                    || !sampleMetadata.getSampleType().equals(expected.getSampleType())
+                    || !sampleMetadata.getSex().equals(expected.getSex())
+                    || !sampleMetadata.getTumorOrNormal().equals(expected.getTumorOrNormal())) {
+                Assert.fail("Resolved values in dmp sample " + dmpSample.getDmpSampleId() + " do not match "
+                        + "expected values in: " + expected.toString());
+            }
         }
+    }
+
+    private Map<String, SampleMetadata> initExpectedConvertedDmpSampleValues() {
+        Map<String, SampleMetadata> map = new HashMap<>();
+        map.putAll(getSampleMetadataMapEntry("P-0000001-N01-IM3", "Normal", "Primary", "Male", "2019"));
+        map.putAll(getSampleMetadataMapEntry("P-0000001-T01-IM3", "Tumor", "Metastasis", "Male", "2019"));
+        map.putAll(getSampleMetadataMapEntry("P-0000002-N01-IM3", "Normal", "Primary", "Female", "2020"));
+        map.putAll(getSampleMetadataMapEntry("P-0000002-T01-IM3", "Tumor", "Primary", "Female", "2020"));
+        map.putAll(getSampleMetadataMapEntry("P-0000222-N01-IM3", "Normal", "Primary", "Female", "2019"));
+        map.putAll(getSampleMetadataMapEntry("P-0000222-T01-IM3", "Tumor", "Primary", "Female", "2019"));
+        map.putAll(getSampleMetadataMapEntry("P-0000333-N01-IM3", "Normal", "Primary", "Male", "2019"));
+        map.putAll(getSampleMetadataMapEntry("P-0000333-T01-IM3", "Tumor", "Primary", "Male", "2019"));
+        map.putAll(getSampleMetadataMapEntry("P-0004000-N01-IM3", "Normal", "Primary", "Male", "2019"));
+        map.putAll(getSampleMetadataMapEntry("P-0004000-T01-IM3", "Tumor", "Primary", "Male", "2019"));
+        map.putAll(getSampleMetadataMapEntry("P-0004444-N01-IM3", "Normal", "Primary", "Female", "2020"));
+        map.putAll(getSampleMetadataMapEntry("P-0007111-N01-IM3", "Normal", "Primary", "Male", "2017"));
+        map.putAll(getSampleMetadataMapEntry("P-0007111-T01-IM3", "Tumor", "Metastasis", "Male", "2017"));
+        map.putAll(getSampleMetadataMapEntry("P-0008999-N01-IM3", "Normal", "Primary", "Female", "2019"));
+        map.putAll(getSampleMetadataMapEntry("P-0008999-T01-IM3", "Tumor", "Primary", "Female", "2019"));
+        map.putAll(getSampleMetadataMapEntry("P-6660000-N01-IM3", "Normal", "Primary", "Female", "2020"));
+        map.putAll(getSampleMetadataMapEntry("P-7778999-N01-IM3", "Normal", "Primary", "Female", "2018"));
+        map.putAll(getSampleMetadataMapEntry("P-7778999-T01-IM3", "Tumor", "Primary", "Female", "2018"));
+        map.putAll(getSampleMetadataMapEntry("P-8882444-N01-IM3", "Normal", "Primary", "Male", "2018"));
+        map.putAll(getSampleMetadataMapEntry("P-8882444-T01-IM3", "Tumor", "Primary", "Male", "2018"));
+        map.putAll(getSampleMetadataMapEntry("P-9990000-N01-IM3", "Normal", "Primary", "Female", "2019"));
+        map.putAll(getSampleMetadataMapEntry("P-9990000-T01-IM3", "Tumor", "Metastasis", "Female", "2019"));
+        map.putAll(getSampleMetadataMapEntry("P-9992811-N01-IM3", "Normal", "Primary", "Male", "2018"));
+        map.putAll(getSampleMetadataMapEntry("P-9992811-T01-IM3", "Tumor", "Metastasis", "Male", "2018"));
+        map.putAll(getSampleMetadataMapEntry("P-9998808-N01-IM3", "Normal", "Primary", "Female", "2018"));
+        map.putAll(getSampleMetadataMapEntry("P-9998808-T01-IM3", "Tumor", "Primary", "Female", "2018"));
+        return map;
+    }
+
+    private Map<String, SampleMetadata> getSampleMetadataMapEntry(String dmpId, String sampleClass,
+            String sampleType, String sex, String collectionYear) {
+        SampleMetadata sm = new SampleMetadata();
+        sm.setPrimaryId(dmpId);
+        sm.setSampleClass(sampleClass);
+        sm.setSampleType(sampleType);
+        sm.setSex(sex);
+        sm.setTumorOrNormal(sampleClass);
+        sm.setCollectionYear(collectionYear);
+
+        Map<String, SampleMetadata> map = new HashMap<>();
+        map.put(dmpId, sm);
+        return map;
     }
 }
