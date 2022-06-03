201b6d319fff82e2a7fd07578321c27199cb8963
Divya Madala <71040191+divyamadala30@users.noreply.github.com>
2022-06-03 12:19:36 -0400

    worsking state, checkstyle fix
    Signed-off-by: Divya Madala <71040191+divyamadala30@users.noreply.github.com>

--- service/src/main/java/org/mskcc/smile/service/impl/RequestServiceImpl.java
+++ service/src/main/java/org/mskcc/smile/service/impl/RequestServiceImpl.java
@@ -65,7 +65,7 @@ public class RequestServiceImpl implements SmileRequestService {
         project.setNamespace(request.getNamespace());
         request.setSmileProject(project);
 
-        SmileRequest savedRequest = requestRepository.findRequestById(request.getIgoRequestId());
+        SmileRequest savedRequest = getSmileRequestById(request.getIgoRequestId());;
         if (savedRequest == null) {
             if (request.getSmileSampleList() != null) {
                 List<SmileSample> updatedSamples = new ArrayList<>();
@@ -78,17 +78,17 @@ public class RequestServiceImpl implements SmileRequestService {
             }
             requestRepository.save(request);
             return Boolean.TRUE;
-        } else if ((requestHasMetadataUpdates(savedRequest.getLatestRequestMetadata(), request.getLatestRequestMetadata()))
+        } else if ((requestHasMetadataUpdates(savedRequest.getLatestRequestMetadata(),
+                request.getLatestRequestMetadata()))
                 || savedRequest.getSmileSampleList().size() != request.getSmileSampleList().size()) {
             LOG.info("Persisting updates for request: " + savedRequest.getIgoRequestId());
             savedRequest.updateRequestMetadataByMetadata(request.getLatestRequestMetadata());
             //Check if there are sampleUpdates
+            List<SmileSample> updatedSamples = new ArrayList<>();
             for (SmileSample sample: request.getSmileSampleList()) {
-                if (!savedRequest.getSmileSampleList().contains(sample)) {
-                    savedRequest.getSmileSampleList().add(sample);
-                }
-                sampleService.saveSmileSample(sample);
+                updatedSamples.add(sampleService.saveSmileSample(sample));
             }
+            savedRequest.setSmileSampleList(updatedSamples);
             saveRequestMetadata(savedRequest);
             return Boolean.TRUE;
         }
@@ -242,7 +242,7 @@ public class RequestServiceImpl implements SmileRequestService {
             if (existingSample == null) {
                 continue;
             }
-            Boolean sampleHasUpdates = 
+            Boolean sampleHasUpdates =
                     sampleService.sampleHasMetadataUpdates(existingSample.getLatestSampleMetadata(),
                     sample.getLatestSampleMetadata(), Boolean.TRUE);
             if (sampleHasUpdates) {
