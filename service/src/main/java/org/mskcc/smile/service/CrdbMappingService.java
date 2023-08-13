95b89a6943b4697459785285a2dc5adfe6327d35
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2023-08-13 16:35:03 -0400

    Bug fix for CORRECT_CMO_PATIENT_ID workflow
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

--- service/src/main/java/org/mskcc/smile/service/CrdbMappingService.java
+++ service/src/main/java/org/mskcc/smile/service/CrdbMappingService.java
@@ -4,6 +4,6 @@ import org.mskcc.smile.model.internal.CrdbMappingModel;
 
 public interface CrdbMappingService {
     String getCmoPatientIdbyDmpId(String dmpId);
-    String getCmoPatientIdByInputId(String inputId);
+    String getCmoPatientIdByInputId(String inputId) throws Exception;
     CrdbMappingModel getCrdbMappingModelByInputId(String inputId) throws Exception;
 }
