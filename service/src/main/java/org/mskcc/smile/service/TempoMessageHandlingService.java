9fbbd884d3b98b665175b05844ac88513f22efa3
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2024-02-08 16:01:53 -0500

    QC complete handler
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

--- service/src/main/java/org/mskcc/smile/service/TempoMessageHandlingService.java
+++ service/src/main/java/org/mskcc/smile/service/TempoMessageHandlingService.java
@@ -3,6 +3,7 @@ package org.mskcc.smile.service;
 import java.util.Map;
 import org.mskcc.cmo.messaging.Gateway;
 import org.mskcc.smile.model.tempo.BamComplete;
+import org.mskcc.smile.model.tempo.QcComplete;
 
 /**
  *
@@ -11,5 +12,6 @@ import org.mskcc.smile.model.tempo.BamComplete;
 public interface TempoMessageHandlingService {
     void intialize(Gateway gateway) throws Exception;
     void bamCompleteHandler(Map.Entry<String, BamComplete> bcEvent) throws Exception;
+    void qcCompleteHandler(Map.Entry<String, QcComplete> bcEvent) throws Exception;
     void shutdown() throws Exception;
 }
--- service/src/main/java/org/mskcc/smile/service/impl/TempoMessageHandlingServiceImpl.java
+++ service/src/main/java/org/mskcc/smile/service/impl/TempoMessageHandlingServiceImpl.java
@@ -18,6 +18,7 @@ import org.apache.commons.logging.LogFactory;
 import org.mskcc.cmo.messaging.Gateway;
 import org.mskcc.cmo.messaging.MessageConsumer;
 import org.mskcc.smile.model.tempo.BamComplete;
+import org.mskcc.smile.model.tempo.QcComplete;
 import org.mskcc.smile.model.tempo.Tempo;
 import org.mskcc.smile.service.SmileSampleService;
 import org.mskcc.smile.service.TempoMessageHandlingService;
@@ -34,6 +35,9 @@ import org.springframework.stereotype.Component;
 public class TempoMessageHandlingServiceImpl implements TempoMessageHandlingService {
     @Value("${tempo.wes_bam_complete_topic}")
     private String TEMPO_WES_BAM_COMPLETE_TOPIC;
+
+    @Value("${tempo.wes_qc_complete_topic}")
+    private String TEMPO_WES_QC_COMPLETE_TOPIC;
 
     @Value("${num.tempo_msg_handler_threads}")
     private int NUM_TEMPO_MSG_HANDLERS;
@@ -53,8 +57,11 @@ public class TempoMessageHandlingServiceImpl implements TempoMessageHandlingServ
     private static final ExecutorService exec = Executors.newCachedThreadPool();
     private static final BlockingQueue<Map.Entry<String, BamComplete>> bamCompleteQueue =
             new LinkedBlockingQueue<Map.Entry<String, BamComplete>>();
+    private static final BlockingQueue<Map.Entry<String, QcComplete>> qcCompleteQueue =
+            new LinkedBlockingQueue<Map.Entry<String, QcComplete>>();
 
     private static CountDownLatch bamCompleteHandlerShutdownLatch;
+    private static CountDownLatch qcCompleteHandlerShutdownLatch;
 
     private class BamCompleteHandler implements Runnable {
         final Phaser phaser;
@@ -93,7 +100,45 @@ public class TempoMessageHandlingServiceImpl implements TempoMessageHandlingServ
                 }
             }
         }
+    }
 
+    private class QcCompleteHandler implements Runnable {
+        final Phaser phaser;
+        boolean interrupted = false;
+
+        QcCompleteHandler(Phaser phaser) {
+            this.phaser = phaser;
+        }
+
+        @Override
+        public void run() {
+            phaser.arrive();
+            while (true) {
+                try {
+                    Entry<String, QcComplete> qcEvent = qcCompleteQueue.poll(100, TimeUnit.MILLISECONDS);
+                    if (qcEvent != null) {
+                        // first determine if sample exists by the provided primary id
+                        String primaryId = qcEvent.getKey();
+                        QcComplete qcComplete = qcEvent.getValue();
+                        if (sampleService.sampleExistsByPrimaryId(primaryId)) {
+                            // merge and/or create tempo qc complete event to sample
+                            Tempo tempo = tempoService.getTempoDataBySamplePrimaryId(primaryId);
+                            if (tempo == null
+                                    || !tempo.hasQcCompleteEvent(qcComplete)) {
+                                tempoService.mergeQcCompleteEventBySamplePrimaryId(primaryId,
+                                        qcComplete);
+                            }
+                        } else {
+                            LOG.error("Sample with primary id: " + primaryId + " does not exist");
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    interrupted = true;
+                } catch (Exception e) {
+                    LOG.error("Error during handling of BAM complete event", e);
+                }
+            }
+        }
     }
 
     @Override
@@ -101,6 +146,7 @@ public class TempoMessageHandlingServiceImpl implements TempoMessageHandlingServ
         if (!initialized) {
             messagingGateway = gateway;
             setupBamCompleteHandler(messagingGateway, this);
+            setupQcCompleteHandler(messagingGateway, this);
             initializeMessageHandlers();
             initialized = true;
         } else {
@@ -122,12 +168,26 @@ public class TempoMessageHandlingServiceImpl implements TempoMessageHandlingServ
     }
 
     @Override
+    public void qcCompleteHandler(Map.Entry<String, QcComplete> qcEvent) throws Exception {
+        if (!initialized) {
+            throw new IllegalStateException("Message Handling Service has not been initialized");
+        }
+        if (!shutdownInitiated) {
+            qcCompleteQueue.put(qcEvent);
+        } else {
+            LOG.error("Shutdown initiated, not accepting QC event: " + qcEvent);
+            throw new IllegalStateException("Shutdown initiated, not handling any more TEMPO events");
+        }
+    }
+
+    @Override
     public void shutdown() throws Exception {
         if (!initialized) {
             throw new IllegalStateException("Message Handling Service has not been initialized");
         }
         exec.shutdownNow();
         bamCompleteHandlerShutdownLatch.await();
+        qcCompleteHandlerShutdownLatch.await();
         shutdownInitiated = true;
     }
 
@@ -141,6 +201,16 @@ public class TempoMessageHandlingServiceImpl implements TempoMessageHandlingServ
             exec.execute(new BamCompleteHandler(bamCompletePhaser));
         }
         bamCompletePhaser.arriveAndAwaitAdvance();
+
+        // qc complete handler
+        qcCompleteHandlerShutdownLatch = new CountDownLatch(NUM_TEMPO_MSG_HANDLERS);
+        final Phaser qcCompletePhaser = new Phaser();
+        qcCompletePhaser.register();
+        for (int lc = 0; lc < NUM_TEMPO_MSG_HANDLERS; lc++) {
+            qcCompletePhaser.register();
+            exec.execute(new QcCompleteHandler(qcCompletePhaser));
+        }
+        qcCompletePhaser.arriveAndAwaitAdvance();
     }
 
     private void setupBamCompleteHandler(Gateway gateway,
@@ -162,6 +232,31 @@ public class TempoMessageHandlingServiceImpl implements TempoMessageHandlingServ
                 } catch (Exception e) {
                     LOG.error("Exception occurred during processing of BAM complete event: "
                             + TEMPO_WES_BAM_COMPLETE_TOPIC, e);
+                }
+            }
+        });
+    }
+
+    private void setupQcCompleteHandler(Gateway gateway,
+            TempoMessageHandlingService tempoMessageHandlingService) throws Exception {
+        gateway.subscribe(TEMPO_WES_QC_COMPLETE_TOPIC, Object.class, new MessageConsumer() {
+            @Override
+            public void onMessage(Message msg, Object message) {
+                try {
+                    String qcCompleteJson = mapper.readValue(
+                        new String(msg.getData(), StandardCharsets.UTF_8),
+                        String.class);
+                        Map<String, String> qcCompleteMap = mapper.readValue(qcCompleteJson, Map.class);
+                        QcComplete qcComplete = new QcComplete(qcCompleteMap.get("date"),
+                                qcCompleteMap.get("result"), qcCompleteMap.get("reason"),
+                                qcCompleteMap.get("status"));
+                    String primaryId = qcCompleteMap.get("primaryId");
+                    Map.Entry<String, QcComplete> eventData =
+                            new AbstractMap.SimpleImmutableEntry<>(primaryId, qcComplete);
+                    tempoMessageHandlingService.qcCompleteHandler(eventData);
+                } catch (Exception e) {
+                    LOG.error("Exception occurred during processing of QC complete event: "
+                            + TEMPO_WES_QC_COMPLETE_TOPIC, e);
                 }
             }
         });
