3b2e24d1ee89baf1a7652bef9273c8d51abe8385
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2024-09-20 17:42:56 -0400

    Uupdate testcontainers neo4j image
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

--- service/src/test/java/org/mskcc/smile/service/CorrectCmoPatientIdHandlerTest.java
+++ service/src/test/java/org/mskcc/smile/service/CorrectCmoPatientIdHandlerTest.java
@@ -19,6 +19,7 @@ import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.Neo4jContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 /**
  *
@@ -41,7 +42,7 @@ public class CorrectCmoPatientIdHandlerTest {
     private SmilePatientService patientService;
 
     @Container
-    private static final Neo4jContainer<?> databaseServer = new Neo4jContainer<>()
+    private static final Neo4jContainer<?> databaseServer = new Neo4jContainer<>(DockerImageName.parse("neo4j:5.19.0"))
             .withEnv("NEO4J_dbms_security_procedures_unrestricted", "apoc.*,algo.*");
 
     @TestConfiguration
--- service/src/test/java/org/mskcc/smile/service/RequestServiceTest.java
+++ service/src/test/java/org/mskcc/smile/service/RequestServiceTest.java
@@ -20,6 +20,7 @@ import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.Neo4jContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
 @DataNeo4jTest
@@ -41,7 +42,7 @@ public class RequestServiceTest {
     private TempoService tempoService;
 
     @Container
-    private static final Neo4jContainer<?> databaseServer = new Neo4jContainer<>()
+    private static final Neo4jContainer<?> databaseServer = new Neo4jContainer<>(DockerImageName.parse("neo4j:5.19.0"))
             .withEnv("NEO4J_dbms_security_procedures_unrestricted", "apoc.*,algo.*");
 
     @TestConfiguration
