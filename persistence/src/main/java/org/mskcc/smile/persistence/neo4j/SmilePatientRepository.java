9a03877d30e388b1ab05ecb37cb52b7339f0ccc8
ao508 <15623749+ao508@users.noreply.github.com>
2025-07-18 12:39:56 -0700

    Change @Query strings to text blocks (#1608)
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

--- persistence/src/main/java/org/mskcc/smile/persistence/neo4j/SmilePatientRepository.java
+++ persistence/src/main/java/org/mskcc/smile/persistence/neo4j/SmilePatientRepository.java
@@ -13,30 +13,42 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface SmilePatientRepository extends Neo4jRepository<SmilePatient, Long> {
-    @Query("MATCH (p: Patient {smilePatientId: $smilePatientId})<-[ia:IS_ALIAS]-(pa: PatientAlias) "
-            + "RETURN DISTINCT p, ia, pa")
+    @Query("""
+           MATCH (p: Patient {smilePatientId: $smilePatientId})
+           <-[ia:IS_ALIAS]-(pa: PatientAlias)
+           RETURN DISTINCT p, ia, pa
+           """)
     SmilePatient findPatientByPatientSmileId(@Param("smilePatientId") UUID smileSampleId);
 
-    @Query("OPTIONAL MATCH (s: Sample {smileSampleId: $smileSampleId})<-[hs:HAS_SAMPLE]-(p: Patient)"
-            + "<-[ia:IS_ALIAS]-(pa: PatientAlias) "
-            + "RETURN DISTINCT p, hs, ia, pa")
+    @Query("""
+           OPTIONAL MATCH (s: Sample {smileSampleId: $smileSampleId})<-[hs:HAS_SAMPLE]-(p: Patient)
+           <-[ia:IS_ALIAS]-(pa: PatientAlias)
+           RETURN DISTINCT p, hs, ia, pa
+           """)
     SmilePatient findPatientBySampleSmileId(@Param("smileSampleId") UUID smileSampleId);
 
-    @Query("MATCH (p: Patient)<-[:IS_ALIAS]-(pa: PatientAlias) "
-            + "WHERE pa.namespace = 'cmoId' AND pa.value = $cmoPatientId "
-            + "MATCH (p)<-[ipa:IS_ALIAS]-(pa2: PatientAlias) "
-            + "RETURN DISTINCT p, ipa, pa2")
+    @Query("""
+           MATCH (p: Patient)<-[:IS_ALIAS]-(pa: PatientAlias)
+           WHERE pa.namespace = 'cmoId' AND pa.value = $cmoPatientId
+           MATCH (p)<-[ipa:IS_ALIAS]-(pa2: PatientAlias)
+           RETURN DISTINCT p, ipa, pa2
+           """)
     SmilePatient findPatientByCmoPatientId(
             @Param("cmoPatientId") String cmoPatientId);
 
-    @Query("MATCH (p: Patient)<-[:IS_ALIAS]-(pa: PatientAlias {value: $oldCmoId, namespace: 'cmoId'}) "
-            + "SET pa.value = $newCmoId WITH p "
-            + "MATCH (p)<-[ipa:IS_ALIAS]-(pa2: PatientAlias) "
-            + "RETURN DISTINCT p, ipa, pa2")
+    @Query("""
+           MATCH (p: Patient)<-[:IS_ALIAS]-(pa: PatientAlias {value: $oldCmoId, namespace: 'cmoId'})
+           SET pa.value = $newCmoId
+           WITH p
+           MATCH (p)<-[ipa:IS_ALIAS]-(pa2: PatientAlias)
+           RETURN DISTINCT p, ipa, pa2
+           """)
     SmilePatient updateCmoPatientIdInPatientNode(@Param("oldCmoId") String oldCmoId,
             @Param("newCmoId") String newCmoId);
 
-    @Query("MATCH (p: Patient {smilePatientId: $patient.smilePatientId})"
-            + "<-[:IS_ALIAS]-(pa: PatientAlias) DETACH DELETE p, pa")
+    @Query("""
+           MATCH (p: Patient {smilePatientId: $patient.smilePatientId})<-[:IS_ALIAS]-(pa: PatientAlias)
+           DETACH DELETE p, pa
+           """)
     void deletePatientAndAliases(@Param("patient") SmilePatient patient);
 }
